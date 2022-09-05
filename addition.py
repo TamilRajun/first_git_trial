a97f2ff078338199034a5f546d38919b99eac060
Tamil Arasi <tamilarasi87thulasi@gmail.com>
2022-09-05 05:45:23 +0530

    Second Commit Adding division

--- addition.py
+++ addition.py
@@ -1,8 +1,15 @@
-
+
+
 num1 = input("Enter the Two numbers\n")
 num2 = input("Enter the Two numbers\n")
 print(f'{int(num1) + int(num2) = }')
 
 num1 = input("Enter the Two numbers\n")
 num2 = input("Enter the Two numbers\n")
-print(f'{int(num1) * int(num2) = }')
+print(f'{int(num2) * int(num2) = }')
+
+num1 = input("Enter the Two Numbers\n")
+num2 = input("Enter the Two numbers\n")
+print(f'{int(num2) / int(num2) = }')
+
+
