6afe007f02c18f4c21488da6ac8c2ccf6f2b2e0d
Tamil Arasi <tamilarasi87thulasi@gmail.com>
2022-09-05 05:31:25 +0530

    Creating addition python file

---
+++ addition.py
@@ -0,0 +1,8 @@
+
+num1 = input("Enter the Two numbers\n")
+num2 = input("Enter the Two numbers\n")
+print(f'{int(num1) + int(num2) = }')
+
+num1 = input("Enter the Two numbers\n")
+num2 = input("Enter the Two numbers\n")
+print(f'{int(num1) * int(num2) = }')
